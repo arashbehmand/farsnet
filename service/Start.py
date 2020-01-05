ba19a6067fbde75b79996335256225ea8b729a6b
Arash <behmand@divar.ir>
2020-01-05 17:04:58 +0330

    add manifest.in change directory tree

--- service/Start.py
+++
@@ -1,9 +0,0 @@
-class Start(object):
-    @classmethod
-    def main(cls, args):
-        print "Welcome to Farsnet3 API"
-
-
-if __name__ == "__main__":
-    import sys
-    Start.main(sys.argv)
--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='farsnet', version='1.0', packages=find_packages(),package_data={
-        '': ['farsnet2.5.db3'],
-    })
+setup(
+    name="farsnet",
+    version="1.0",
+    packages=find_packages(),
+    package_data = {
+    'farsnet' : ['farsnet2.5.db3'],
+    },
+    include_package_data = True
+)
+
